203faec6645d1ba8194556b06f821f4343e059ab
jhserodio <jhserodio>
2019-07-17 17:05:13 -0300

    feat (pages): start pages directory

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import style from './Home.module.css';
+
+const Home = () => <div className={style.Home}>Home</div>;
+
+export { Home };
---
+++ src/pages/Tech/Tech.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import style from './Tech.module.css';
+
+const Tech = () => <div className={style.Tech}>Tech</div>;
+
+export { Tech };
