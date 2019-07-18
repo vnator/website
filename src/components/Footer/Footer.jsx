c435d23bb0bbf2369634d76a93fa48defa2e36f5
jhserodio <jhserodio>
2019-07-18 16:26:14 -0300

    feat (components): update Footer
    Add Content, external links and aria-roles in src/components/Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,15 +2,31 @@ import React from 'react';
 import { injectIntl, intlShape } from 'react-intl';
 
 import style from './Footer.module.css';
+import { SOCIAL } from '../../constants/externalLinks';
 
 let Footer = ({ intl: { formatMessage } }) => (
-  <div className={style.Footer}>{formatMessage({ id: 'footer.title' })}</div>
+  <footer className={style.Footer}>
+    <small>&copy; {formatMessage({ id: 'footer.copyRight' })}</small>
+    <ul>
+      {Object.values(SOCIAL).map(link => (
+        <li>
+          <a
+            {...link}
+            aria-label={formatMessage({
+              id: `footer.externalLinks.${link.rel}`,
+            })}>
+            icon {link.rel}
+          </a>
+        </li>
+      ))}
+    </ul>
+  </footer>
 );
-
-Footer = injectIntl(Footer);
 
 Footer.propTypes = {
   intl: intlShape.isRequired,
 };
+
+Footer = injectIntl(Footer);
 
 export { Footer };
