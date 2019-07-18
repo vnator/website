9be870d48101ff94f6e4a7251ecdf03e6271276c
jhserodio <jhserodio>
2019-07-18 14:34:14 -0300

    feat (constants): add elementAttr

---
+++ src/constants/elementAttr.js
@@ -0,0 +1,20 @@
+const ID = Object.freeze({
+  HEADER: 'header',
+  MENU: 'menu',
+  MENU_OPEN: 'menu-open',
+  FOOTER: 'FOOTER',
+});
+
+const ROLE = Object.freeze({
+  HEADING: 'heading',
+  NAVIGATION: 'navigation',
+  MENU: 'menu',
+  MENU_ITEM: 'menuitem',
+  BANNER: 'banner',
+  MAIN: 'main',
+  IMG: 'img',
+  FORM: 'form',
+  NONE: 'none',
+});
+
+export { ID, ROLE };
