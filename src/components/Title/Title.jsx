087bd669ac3c95c8ae93a9253b43210060468998
jhserodio <jhserodio@gmail.com>
2019-08-01 10:25:49 -0300

    feat (pages/Tech): update content

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -5,7 +5,7 @@ import style from './Title.module.css';
 import { ROLE } from '../../constants/elementAttr';
 
 const Title = ({ children }) => (
-  <h2 role={ROLE.HEADING} aria-leve={2} className={style.Title}>
+  <h2 role={ROLE.HEADING} aria-level={2} className={style.Title}>
     {children}
   </h2>
 );
--- src/pages/Tech/Tech.jsx
+++ src/pages/Tech/Tech.jsx
@@ -2,6 +2,22 @@ import React from 'react';
 
 import style from './Tech.module.css';
 
-const Tech = () => <div className={style.Tech}>Tech</div>;
+const Tech = () => (
+  <div className={style.Tech}>
+    Tech
+    <a
+      frameBorder="0"
+      data-theme="light"
+      data-layers="3,2,1"
+      data-stack-embed="true"
+      href="https://embed.stackshare.io/stacks/embed/b8eaa84186bdfd2ea0c54fa4d17238"
+    />
+    <script
+      async
+      src="https://cdn1.stackshare.io/javascripts/client-code.js"
+      charSet="utf-8"
+    />
+  </div>
+);
 
 export { Tech };
