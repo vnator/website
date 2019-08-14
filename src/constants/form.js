1570e922fc2de435813fbcd3fb9db9951dd5d8e8
jhserodio <jhserodio@gmail.com>
2019-08-14 10:54:13 -0300

    feat (Contact): add Contact content

---
+++ src/constants/form.js
@@ -0,0 +1,20 @@
+const FORM = Object.freeze({
+  EMAIL: 'email',
+  SUBJECT: 'subject',
+  NAME: 'name',
+  MESSAGE: 'message',
+});
+
+const TYPE = Object.freeze({
+  TEXT: 'text',
+  EMAIL: 'email',
+});
+
+const HINT = Object.freeze({
+  MIN: 'MIN',
+  MAX: 'MAX',
+  EMAIL: 'EMAIL',
+  REQUIRED: 'REQUIRED',
+});
+
+export { FORM, TYPE, HINT };
--- src/pages/Contact/intl.ptBR.js
+++ src/pages/Contact/intl.ptBR.js
@@ -1,5 +1,34 @@
+import { FORM, HINT } from '../../constants/form';
+
 const contact_ptBR = {
-  title: 'Contact page',
+  title: 'Contato',
+  form: {
+    legend: 'Gostaria de nos deixar uma mensagem?',
+    input: {
+      [FORM.EMAIL]: {
+        label: 'E-mail',
+        placeholder: 'Digite seu e-mail',
+      },
+      [FORM.NAME]: {
+        label: 'Nome',
+        placeholder: 'Digite seu nome completo',
+      },
+      [FORM.SUBJECT]: {
+        label: 'Assunto',
+        placeholder: 'Qual o motivo da sua mensagem?',
+      },
+      [FORM.MESSAGE]: {
+        label: 'Mensagem',
+        placeholder: 'Deixe sua mensagem',
+      },
+    },
+    hint: {
+      [HINT.EMAIL]: 'digite um e-mail valido',
+      [HINT.MIN]: 'mínimo {number} caracteres',
+      [HINT.MAX]: '{current}/{max}',
+      [HINT.REQUIRED]: 'campo obrigatório',
+    },
+  },
 };
 
 export { contact_ptBR };
