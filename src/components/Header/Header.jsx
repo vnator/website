66878857b46d0b61be74684a1c4a3a56500e3629
jhserodio <jhserodio@gmail.com>
2019-08-17 17:54:18 -0300

    feat (Header): upadte header style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,30 +1,38 @@
 import React from 'react';
+import Type from 'prop-types';
 import { useIntl } from 'react-intl';
 
 import style from './Header.module.css';
 import { Logo } from '../Logo/Logo';
 import { ID, ROLE } from '../../constants/elementAttr';
 
-let Header = () => {
+const Header = ({ openSidebar, isOpen }) => {
   const { formatMessage } = useIntl();
   return (
     <header className={style.Header}>
       <h1 className={style.pageTitle} role={ROLE.HEADING} aria-level={1}>
-        <Logo width="auto" height="120px" />
+        <Logo className={style.logo} />
       </h1>
-
+      <div className={style.inDevelopmentAlert}>in development</div>
       <button
         className={`_reset ${style.button}`}
         id={ID.MENU_OPEN}
         aria-haspopup={true}
-        aria-expanded={true}
+        aria-expanded={isOpen}
         aria-label={formatMessage({ id: 'header.button' })}
+        onClick={openSidebar}
         aria-controls={ID.MENU}>
-        open Sidebar
+        <span className={`${style.bar} ${style._left}`} />
+        <span className={`${style.bar} ${style._right}`} />
+        <span className={`${style.bar} ${style._left}`} />
       </button>
-
-      <div className={style.inDevelopmentAlert}>page in development</div>
     </header>
   );
 };
+
+Header.propTypes = {
+  openSidebar: Type.func.isRequired,
+  isOpen: Type.bool.isRequired,
+};
+
 export { Header };
